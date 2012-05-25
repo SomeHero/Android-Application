c09dc9252acea62614efa872c1e71eb83a53a901
Edward Mitchell <edward@pdthx.me>
2012-05-25 14:45:43 -0400

    Signed-off-by: Edward Mitchell <edward@pdthx.me>

--- src/me/pdthx/Requests/UserSetupSecurityPinRequest.java
+++
@@ -1,9 +0,0 @@
-package me.pdthx.Requests;
-
-public class UserSetupSecurityPinRequest {
-	public UserSetupSecurityPinRequest() { }
-	
-	public String UserId = "";
-	public String DeviceId = "";
-	public String SecurityPin = "";
-}
