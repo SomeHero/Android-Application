b0c1ed8fc6c8485baa1bf652de92c1cd01b4c2b9
Ivy Jeong <ivy@pdthx.me>
2012-06-12 11:33:20 -0400

    Ivy's Changes

---
+++ src/me/pdthx/Login/AboutActivity.java
@@ -0,0 +1,13 @@
+package me.pdthx.Login;
+
+import me.pdthx.R;
+import android.app.Activity;
+import android.os.Bundle;
+
+public class AboutActivity extends Activity {
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.about);
+	}
+}
